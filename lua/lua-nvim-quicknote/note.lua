f8b0ccbb0c98951efd3b5d0a6473b3b04e2a70ff
Brandon Bouchard <58348396+Bottle7@users.noreply.github.com>
2024-08-11 13:17:42 -0700

    Fixes syntax error

--- lua/lua-nvim-quicknote/note.lua
+++ lua/lua-nvim-quicknote/note.lua
@@ -34,7 +34,7 @@ local function load_config()
 end
 
 local function sanitize_filename(name)
-  name = name.gsub('[^%w%s%-]', '_')
+  name = name:gsub('[^%w%s%-]', '_')
   name = name:gsub('%s', '_')
   name = name:gsub('^_+', ''):gsub('_+$', '')
   return name
